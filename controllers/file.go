69ce9626ca9e05d64a6e8eeb63f0bedd763a0633
xiongjinchao <xiongjinchao@funhan.cn>
2019-07-23 20:12:34 +0800

    carry on

--- controllers/file.go
+++ controllers/file.go
@@ -1,17 +1,43 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 type File struct{}
 
 // Upload handles POST /admin/file/upload route
 func (_ *File) Upload(c *gin.Context) {
-	c.HTML(http.StatusOK, "home/index", gin.H{
-		"title": "文件上传",
-	})
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failure",
+			"message": err.Error(),
+			"data":    "",
+		})
+		return
+	}
+	//文件的名称
+	filename := header.Filename
+
+	fmt.Println(file, err, filename)
+	//创建文件
+	out, err := os.Create("static/uploadfile/" + filename)
+	//注意此处的 static/uploadfile/ 不是/static/uploadfile/
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.String(http.StatusCreated, "upload successful")
 }
 
 // Delete handles GET /admin/file/delete route
