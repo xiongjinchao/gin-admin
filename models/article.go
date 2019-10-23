cb4db4b016eeefcb8e18428e2eae04488e8a428a
xiongjinchao <xiongjinchao@hotmail.com>
2019-10-24 01:49:41 +0800

    carry on

--- models/article.go
+++ models/article.go
@@ -1,11 +1,9 @@
 package models
 
 import (
-	"encoding/json"
 	"fmt"
 	db "gin-admin/database"
 	"github.com/gin-gonic/gin"
-	"strconv"
 )
 
 type Article struct {
@@ -45,125 +43,4 @@ func (a *Article) SetTags(articles *[]Article) {
 			v.Tags = nil
 		}
 	}
-}
-
-// get the newest article from cache
-func (a *Article) GetNewNote() (articles []Article, err error) {
-
-	data, err := db.Redis.Get("new-note").Result()
-	if data == "" || err != nil {
-		return a.SetNewNote()
-	}
-
-	if err := json.Unmarshal([]byte(data), &articles); err != nil {
-		return a.SetNewNote()
-	}
-
-	return
-}
-
-// cache the newest article in redis
-func (a *Article) SetNewNote() (articles []Article, err error) {
-
-	if err := db.Mysql.Model(Article{}).
-		Select("id,title,created_at").
-		Where("audit = 1 and category_id in (3,4,5,6,7)").
-		Order("id desc").
-		Limit(8).
-		Find(&articles).Error; err != nil {
-		return articles, err
-	}
-
-	if len(articles) == 0 {
-		return
-	}
-
-	article, err := json.Marshal(articles)
-	if err != nil {
-		return
-	}
-
-	db.Redis.Set("new-note", string(article), 600*1000000000)
-	return
-}
-
-// get the newest article from cache
-func (a *Article) GetNewArticle() (articles []Article, err error) {
-
-	data, err := db.Redis.Get("new-article").Result()
-	if data == "" || err != nil {
-		return a.SetNewArticle()
-	}
-
-	if err := json.Unmarshal([]byte(data), &articles); err != nil {
-		return a.SetNewArticle()
-	}
-
-	return
-}
-
-// cache the newest article in redis
-func (a *Article) SetNewArticle() (articles []Article, err error) {
-
-	if err := db.Mysql.Model(Article{}).
-		Select("id,title,created_at").
-		Where("audit = 1 and category_id in (8,9)").
-		Order("id desc").
-		Limit(8).
-		Find(&articles).Error; err != nil {
-		return articles, err
-	}
-
-	if len(articles) == 0 {
-		return
-	}
-
-	article, err := json.Marshal(articles)
-	if err != nil {
-		return
-	}
-
-	db.Redis.Set("new-article", string(article), 600*1000000000)
-	return
-}
-
-// get the related article from cache
-func (a *Article) GetRelatedArticle(ID, categoryID int64) (articles []Article, err error) {
-	data, err := db.Redis.Get("related-article" + strconv.FormatInt(categoryID, 10)).Result()
-	if data == "" || err != nil {
-		return a.SetRelatedArticle(ID, categoryID)
-	}
-
-	if err := json.Unmarshal([]byte(data), &articles); err != nil {
-		return a.SetRelatedArticle(ID, categoryID)
-	}
-
-	return
-}
-
-// cache the related article in redis
-func (a *Article) SetRelatedArticle(ID, categoryID int64) (articles []Article, err error) {
-
-	if err := db.Mysql.Model(Article{}).
-		Select("id,title,cover,category_id,summary,hit,comment,favorite,user_id,created_at").
-		Where("audit = 1 and category_id = ? and ID != ?", categoryID, ID).
-		Preload("File").Preload("User").Preload("ArticleCategory").
-		Order("id desc").
-		Limit(8).
-		Find(&articles).Error; err != nil {
-		return articles, err
-	}
-
-	if len(articles) == 0 {
-		return
-	}
-	a.SetTags(&articles)
-
-	article, err := json.Marshal(articles)
-	if err != nil {
-		return
-	}
-
-	db.Redis.Set("related-article"+strconv.FormatInt(categoryID, 10), string(article), 600*1000000000)
-	return
 }
